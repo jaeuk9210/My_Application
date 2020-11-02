b5556ce85f953269d2e1a60e3cc5fde63fc9df07
정재욱 <jaeuk@jeuke.com>
2020-11-02 15:03:31 +0900

    calc

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,12 +2,65 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etNum1, etNum2;
+    Button btnAdd, btnSub, btnMul, btnDiv;
+    TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNum1 = (EditText)findViewById(R.id.edit_num1);
+        etNum2 = (EditText)findViewById(R.id.edit_num2);
+        btnAdd = (Button)findViewById(R.id.btn_add);
+        btnSub = (Button)findViewById(R.id.btn_sub);
+        btnMul = (Button)findViewById(R.id.btn_mul);
+        btnDiv = (Button)findViewById(R.id.btn_div);
+        tvResult = (TextView)findViewById(R.id.tv_result);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num1 = Integer.parseInt(etNum1.getText().toString());
+                int num2 = Integer.parseInt(etNum2.getText().toString());
+                tvResult.setText("결과:" + (num1+num2));
+            }
+        });
+
+        btnSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num1 = Integer.parseInt(etNum1.getText().toString());
+                int num2 = Integer.parseInt(etNum2.getText().toString());
+                tvResult.setText("결과:" + (num1-num2));
+            }
+        });
+
+        btnMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num1 = Integer.parseInt(etNum1.getText().toString());
+                int num2 = Integer.parseInt(etNum2.getText().toString());
+                tvResult.setText("결과:" + (num1*num2));
+            }
+        });
+
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num1 = Integer.parseInt(etNum1.getText().toString());
+                int num2 = Integer.parseInt(etNum2.getText().toString());
+                tvResult.setText("결과:" + (num1/num2));
+            }
+        });
+
     }
 }
